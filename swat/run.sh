2128aacaab248815745df5aa8f536bc6812e306c
Bruce W. Herr II <bherr@iu.edu>
2026-01-23 09:35:36 -0500

    Updated for v2 swat data

--- swat/run.sh
+++ swat/run.sh
@@ -3,6 +3,11 @@
 export JAVA_OPTS="-Xms2g -Xmx96g"
 
 duckdb -no-stdin -init queries/swat-genes.sql > output-data/swat-genes.csv
+
+# Get the values to paste into .rq files
+# tail -n +2 output-data/swat-genes.csv | tr -d '\r' | awk 'NF{printf "(\"%s\"^^xsd:string)%s", $0, (NR? " ":"")} END{print ""}' > paste-me
+# exit
+
 time ../bin/sparql-select-local.sh ../../hra-kg/dist/blazegraph.jnl queries/swat-hra-gene-evidence.rq output-data/swat-hra-gene-evidence.csv
 time ../bin/sparql-select-local.sh ../../hra-kg/dist/blazegraph.jnl queries/cl-lookup.rq output-data/cl-lookup.csv
 
